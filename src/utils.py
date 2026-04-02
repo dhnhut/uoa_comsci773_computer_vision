06c83c8f8225cdbbe50488ed9aad12c07eee65ec
dhnhut@gmail.com <dhnhut@gmail.com>
2026-04-02 22:52:22 +1300

    Quiz 2

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,3 @@ import numpy as np
 
 def round_matrix(arr: np.ndarray) -> np.ndarray:
     return np.where(arr >= 0, np.floor(arr + 0.5), np.ceil(arr - 0.5)).astype(np.int64)
-
-
-round_matrix(np.array([[-14.75, -1.5, -0.5], [0, 0.5, 1.5]]))
